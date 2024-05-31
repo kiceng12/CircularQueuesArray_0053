51b076648c21d6d5c68ff7414145e27cc14a3949
kiceng12 <sukmahendrawan12@gmail.com>
2024-05-31 14:52:48 +0700

    menulis cek antrian apakah antrian kosong

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -38,7 +38,12 @@ public:
         queue_array[REAR] = num;
     }
 
-    void display() {
-        
+    void remove() {
+        //cek apakah antrian kosong
+        if (FRONT == -1) {
+	        cout << "Queue underflow\n";
+	        return;
+        }
+         cout << "\nlement deleted from the Queue is : " << queue_array[FRONT] << "\n";
     }
 };
